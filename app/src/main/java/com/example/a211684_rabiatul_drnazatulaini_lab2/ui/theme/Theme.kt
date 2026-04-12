3de5dd180cf1a4fdf0a5dc508951cfc6ecd1e3c5
NUR RABIATUL NABILA BINTI TAJUDIN <rabiatulnabila2005@gmail.com>
2026-04-13 01:49:26 +0800

    Restore Lab 2 Version

--- app/src/main/java/com/example/a211684_rabiatul_drnazatulaini_lab2/ui/theme/Theme.kt
+++ app/src/main/java/com/example/a211684_rabiatul_drnazatulaini_lab2/ui/theme/Theme.kt
@@ -36,7 +36,7 @@ private val LightColorScheme = lightColorScheme(
 fun A211684_Rabiatul_DrNazatulAini_Lab2Theme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
